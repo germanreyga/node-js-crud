988aa76272cb6376fb5276b7b9dae0ed59a29d4d
Alejandro Flores <alejandro@esporaestudio.com>
2020-02-09 23:01:53 -0600

    Integración de dotenv

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ let hbs = exphbs.create({extname: extNameHbs});
 app.engine(extNameHbs, hbs.engine);
 app.set('view engine', extNameHbs);
 
+// Importa la configuración de la app
+let appConfig = require('./configs/app');
+
 // Comienza el servidor en el puerto 3000 de localhost
 // para ver el sistema
-app.listen(3000,() => {
-  console.log('App listening on port 3000! (http://localhost:3000)');
+app.listen(appConfig.express_port,() => {
+  let show = 'App listening on port ' + appConfig.express_port + '! (http://localhost:' + appConfig.express_port +')';
+  console.log(show);
 });
