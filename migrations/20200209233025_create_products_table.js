279166fc6e8918967b760e8c177f1ebbf70a76ff
Alejandro Flores <alejandro@esporaestudio.com>
2020-02-09 23:52:22 -0600

    Crea el seed de los productos. Corrige la estructura de la base de datos (timestamps).

--- migrations/20200209233025_create_products_table.js
+++ migrations/20200209233025_create_products_table.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
       table.string('name', 255).notNullable();
       table.string('description', 512).notNullable();
       table.float('price');
-      table.timestamps();
+      table.timestamps(true, true);
     });
 };
 
